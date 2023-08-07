eaa32af5eee6873864f09330a81593ad30d855d9
ikumin3 <primeclover193@gmail.com>
2023-08-08 05:17:15 +0900

    機能追加 ・現在マイクラを遊んでる人数の表示機能
    その他エラーハンドリング追加

--- yakiringo-Chan/main.py
+++ yakiringo-Chan/main.py
@@ -3,6 +3,9 @@ import psutil
 import asyncio
 import os
 from dotenv import load_dotenv
+import subprocess
+import time
+import re
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -15,38 +18,22 @@ class MyClient(discord.Client):
         
     #     if message.content.startswith('$hello'):
     #         await message.channel.send('Hello!')
-            
+    
+    # 非同期javaプロセス監視関数
     async def check_process(self):
         # print(pid)
         # channel = self.get_channel(self.target_channel_id)
 
         while True:
             pid = get_pid('java')
+            player_count = get_member_num()
             if pid is not None and is_process_running(pid):
-                activity = discord.Activity(name='Minecraft is Running　　　', type=discord.ActivityType.watching)
+                activity = discord.Activity(name=f'{player_count} 人が Minecraft ', type=discord.ActivityType.playing)
             else:
                 activity = discord.Activity(name='Minecraft is Stopped　　　', type=discord.ActivityType.watching)
 
             await self.change_presence(activity=activity)
-            await asyncio.sleep(1800)  # Sleep for 1800 seconds
-
-                # process = psutil.Process(pid)
-
-            #     if is_process_running(pid):
-            #         print(f"Process {pid} is runnning")
-            #         # await channel.send(f"起動してます！")
-            #         activity = discord.Activity(name='Minecraft is Running　　　', type=discord.ActivityType.watching)
-            #         await self.change_presence(activity=activity)
-            #     else:
-            #         print(f"Process {pid} is not running")
-            #         activity = discord.Activity(name='Minecraft is Stopped　　　', type=discord.ActivityType.watching)
-            #         await self.change_presence(activity=activity)
-            # except psutil.NoSuchProcess:
-            #     print(f"Process {pid} does not exist.")
-            #     activity = discord.Activity(name='Minecraft is Stopped', type=discord.ActivityType.watching)
-            #     await self.change_presence(activity=activity)
-            #     # await channel.send(f"起動してません！")
-            # await asyncio.sleep(1800)  # Sleep for 5 seconds
+            await asyncio.sleep(300)  # Sleep for 1800 seconds
 
 def get_pid(name):
     for proc in psutil.process_iter(['pid', 'name']):
@@ -60,6 +47,35 @@ def is_process_running(pid):
         return process.is_running()
     except psutil.NoSuchProcess:
         return False
+    
+def get_member_num():
+    try:
+        subprocess.run(["screen","-S","minecraft","-X","stuff","list\n"])
+        time.sleep(5)
+    except:
+        print(f"Error subprocess")
+        return 0
+        
+    try:
+        with open("/home/ubuntu/opt/minecraft/logs/latest.log") as file:
+            lines = file.readlines()
+    except:
+        print(f"Error file open")
+        return 0
+    
+    for line in reversed(lines):
+        print(line)
+        match = None
+        if "There are" in line:
+            try:
+                match = re.search("There are (\d+) of a max of 20 players online:",line)
+            except:
+                print(f"Error match")
+                continue
+        if match:
+            player_count = int(match.group(1))
+            return player_count
+    return 0
 
 intents = discord.Intents.default()
 intents.message_content = True
